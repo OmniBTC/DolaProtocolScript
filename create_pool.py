903bbbb73b1a10f9cd76a97d3b08f4383fefe82d
AAweidai <dw1253464613@gmail.com>
2024-11-04 22:53:45 +0800

    Update script

--- create_pool.py
+++ create_pool.py
@@ -5,10 +5,10 @@ from sui_brownie import sui_brownie
 sui_project = sui_brownie.SuiProject(project_path=Path(__file__).parent, network="sui-mainnet")
 sui_project.active_account("MAIN")
 
-pacakge_id = "0xe030c518163b0f4ed043243bebf85c16743063d98efda5e642a176308c0a54fb"
-escrow_reward = "0xa8371f3bc97b4ed56a35d7bb6d0ec543303f60b5f6721d16833c3d639d7dd30d"
-sui_object = "0xf5f8a63dca4df11b8881b844ca7be98e9a9b934921c88b4331ccb79fb30f1751"
-proposal = "0x702347c1ab4905fbd311198cb6805c30295a13f8c2a429db2efeaa61078b486d"
+pacakge_id = "0x9fe4ad5393f2096bf36ebf5c459311ec79385e0ab899aa5de7ba6c3a54b66106"
+escrow_reward = "0x47364e57f86cafb709e587e15e48e7024b677bfbb8655afbe8aa438827b1c3d8"
+sui_object = "0xf534b2b7de0d908bd7520f32f6de0cb568191929f316e9ba646fd45946e51cba"
+proposal = "0xd9b5e52d196a19ec87733be6fd204bb4020a285a7d0d34aada7ea32e4e7b6df6"
 
 governance_info = "0x79d7106ea18373fc7542b0849d5ebefc3a9daf8b664a4f82d9b35bbd0c22042d"
 storage = "0xe5a189b1858b207f2cf8c05a09d75bae4271c7a9a8f84a8c199c6896dc7c37e6"
@@ -16,7 +16,7 @@ storage = "0xe5a189b1858b207f2cf8c05a09d75bae4271c7a9a8f84a8c199c6896dc7c37e6"
 
 def load_pacakge():
     return sui_brownie.SuiPackage(
-        package_id="0xe030c518163b0f4ed043243bebf85c16743063d98efda5e642a176308c0a54fb",
+        package_id=pacakge_id,
         package_name="Proposal"
     )
 
